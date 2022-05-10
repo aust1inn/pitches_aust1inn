105b86597894767587630301827f423758faa264
aust1inn <austinotieno364@gmail.com>
2022-05-10 08:48:56 +0300

    deployment changes

--- config.py
+++ config.py
@@ -1,21 +1,20 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitches'
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class ProdConfig(Config):
-    uri = os.getenv('DATABASE_URL')
-    if uri and uri.startswith('postgres://'):
-        uri = uri.replace('postgres://', 'postgresql://', 1)
+    # uri = os.getenv('DATABASE_URL')
+    # if uri and uri.startswith('postgres://'):
+    #     uri = uri.replace('postgres://', 'postgresql://', 1)
         
-    SQLALCHEMY_DATABASE_URI=uri    
+    # SQLALCHEMY_DATABASE_URI=uri  
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL').replace ('://', 'ql://', 1)  
 
 
 class DevConfig(Config):
-    # DEBUG = True
-    pass
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitches'
 
 config_options = {
 'development':DevConfig,
